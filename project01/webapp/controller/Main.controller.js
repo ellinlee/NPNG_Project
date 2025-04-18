dcaee157e29d27f0b9d10e7253b2afecdf66feff
ellinlee <luc010302@ajou.ac.kr>
2025-04-18 18:56:50 +0900

    first commit

---
+++ project01/webapp/controller/Main.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+  "use strict";
+
+  return Controller.extend("sync.dc.sd.project01.controller.Main", {
+    onInit() {},
+    onPress() {},
+  });
+});
---
+++ project01/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"syncdcsd/project01/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
