f0554831907ec376a82366e30edde594b6c358ab
ellinlee <elinlee@EunchaeLee>
2025-04-19 15:48:29 +0900

    first display done

--- project01/webapp/controller/Main.controller.js
+++ project01/webapp/controller/Main.controller.js
@@ -1,8 +1,54 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/odata/v2/ODataModel"], 
+  (Controller, ODataModel) => {
   "use strict";
 
   return Controller.extend("sync.dc.sd.project01.controller.Main", {
-    onInit() {},
-    onPress() {},
+    onInit() {
+      var oModel = new ODataModel("/sap/opu/odata/sap/ZDCC_INQUIRYFORM_CDS/");
+
+      this.getView().setModel(oModel);
+
+      oModel.read("/ZDCC_InquiryForm", {
+        success: (oData) => {
+          console.log("Success", oData);
+        },
+        error: (oError) => {
+          console.error("Error", oError);
+        }
+      });
+    },
+    onPress(){
+      var that = this;
+
+      var customerId = this.getView().byId("app_input1").getValue();
+      var oModel1 = this.getView().getModel();
+
+      oModel1.read("/ZDCC_InquiryForm", {
+    filters: [
+      new sap.ui.model.Filter("cust_id", sap.ui.model.FilterOperator.EQ, customerId)
+    ],
+    success: function (oData) {
+      if (oData.results.length > 0) {
+        // 고객 존재 시 라우팅
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(that);
+        oRouter.navTo("RouteSelect", {
+          customerId: encodeURIComponent(customerId)
+        });
+      } else {
+        // 존재하지 않음
+        var oBundle = that.getView().getModel("i18n").getResourceBundle();
+        var sErrorMessage = oBundle.getText("error");
+        sap.m.MessageToast.show(sErrorMessage);
+      }
+    },
+    error: function () {
+      var oBundle = that.getView().getModel("i18n").getResourceBundle();
+      var sErrorMessage = oBundle.getText("error");
+      sap.m.MessageToast.show(sErrorMessage);
+    }
+  });
+    
+    }
   });
 });
+
---
+++ project01/webapp/controller/Select.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+  ], function (Controller) {
+    "use strict";
+  
+    return Controller.extend("sync.dc.sd.project01.controller.Select", {
+      onInit: function () {
+        // 여기에 초기화 로직 작성 가능
+        console.log("Select 컨트롤러 초기화됨");
+      }
+    });
+  });
+  
